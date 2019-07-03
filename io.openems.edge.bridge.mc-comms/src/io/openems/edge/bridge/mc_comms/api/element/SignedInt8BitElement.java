7db8e2b4bbec2bff27ca4fe899ee75bd08458ae8
kyle <kyle@microcare.co.za>
2019-07-03 16:10:52 +0200

    minor change to signed 8 bit integer element value handling

--- io.openems.edge.bridge.mc-comms/src/io/openems/edge/bridge/mc_comms/api/element/SignedInt8BitElement.java
+++ io.openems.edge.bridge.mc-comms/src/io/openems/edge/bridge/mc_comms/api/element/SignedInt8BitElement.java
@@ -3,8 +3,6 @@ package io.openems.edge.bridge.mc_comms.api.element;
 import io.openems.common.types.OpenemsType;
 import io.openems.edge.bridge.mc_comms.util.MCCommsException;
 
-import java.nio.ByteBuffer;
-
 public class SignedInt8BitElement extends Abstract8BitElement<Integer> {
 
     /**
@@ -23,11 +21,7 @@ public class SignedInt8BitElement extends Abstract8BitElement<Integer> {
     @Override
     public void setValue(Integer value) throws MCCommsException {
         if (value < 129 && value > -128) {
-            if (value < 0) {
-                rawValue[0] = ByteBuffer.allocate(4).putInt(256 - ByteBuffer.allocate(4).putInt(value).get(3)).get(3);
-            } else {
-                rawValue[0] = ByteBuffer.allocate(4).putInt(value).get(3);
-            }
+        	rawValue[0] = value.byteValue();
         } else {
             throw new MCCommsException("value must not exceed +128 or -127 for signed 8 bit values");
         }
